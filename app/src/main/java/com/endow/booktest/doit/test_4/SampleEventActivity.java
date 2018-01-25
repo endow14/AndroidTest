773264d6075f46277bbe3f360f4fbc5ab1940e76
endow14 <endow14@naver.com>
2018-01-25 18:17:25 +0900

    미션 6 리소스 변경

---
+++ app/src/main/java/com/endow/booktest/doit/test_4/SampleEventActivity.java
@@ -0,0 +1,17 @@
+package com.endow.booktest.doit.test_4;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by endow14 on 2018. 1. 25..
+ */
+
+public class SampleEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
